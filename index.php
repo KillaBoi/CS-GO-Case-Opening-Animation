02c11b0ccd825fbaca35b3a1e4eceb5aae802243
Smyygel <samppa.pie@gmail.com>
2019-03-18 21:24:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CS:GO Case Opening Animation</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+  <script src="custom.js"></script>
+
+  <link rel="stylesheet" href="style.css">
+
+</head>
+  <body>
+
+    <div class="container-fluid">
+      <div class="row">
+
+        <button type="button" id="openCase" class="btn btn-info copenbtn"> OPEN </button>
+
+        <div class="container-fluid caseOpeningArea">
+          <div class="row">
+
+            <div  class="container animationAreaItems">
+              <div class="row">
+                <div class="row text-center flex-nowrap mx-auto">
+
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn itemBoxAnW">    </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+                    <div class="itemBoxAn">  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </body>
+</html>
